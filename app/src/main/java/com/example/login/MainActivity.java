86375e0d19111caeef5f9f482002f899b8d42681
Dell <tranthingoctuyen.3393@gmail.com>
2024-11-11 07:36:26 +0700

    update lan 1

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.login;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,35 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnddangky,btndangnhap;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.intro);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btndangnhap = findViewById(R.id.btnDangnhap);
+        btnddangky = findViewById(R.id.btnDK);
+
+        btndangnhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myintent = new Intent(MainActivity.this,Loginactivity.class);
+                startActivity(myintent);
+
+            }
+        });
+        btnddangky.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myintent = new Intent(MainActivity.this,Signupactivity.class);
+                startActivity(myintent);
+            }
         });
     }
 }
---
+++ app/src/main/java/com/example/login/UserService.java
@@ -0,0 +1,21 @@
+package com.example.login;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface UserService {
+
+    @GET("api/users")
+    Call<List<User>> getData();
+
+    @POST("api/users")
+    Call<User> Login(@Body User user);
+    @POST("login")
+    Call<User> login(@Body User user);
+
+
+}
